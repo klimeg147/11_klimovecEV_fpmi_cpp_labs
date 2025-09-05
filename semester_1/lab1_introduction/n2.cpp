8e77a8d7d75e46cf526fa2dba17a4df0e7b8470f
EgorKlimovec <klimeg147@gmail.com>
2025-09-05 22:11:09 +0300

    Added file n2.cpp

---
+++ semester_1/lab1_introduction/n2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int ost;
+	long n;
+	long long sum = 0, pr = 1;
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		ost = i % 2;
+		if (ost == 0) {
+			sum = sum + i;
+		}
+		else {
+			pr = pr * i;
+		}
+	}
+	cout << "Sum = " << sum << "\n";
+	cout << "Product = " << pr << "\n";
+}
