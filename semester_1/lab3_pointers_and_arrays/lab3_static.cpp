3be66a1c0402eb191b4efbfdc481b3b4495c7c22
EgorKlimovec <klimeg147@gmail.com>
2025-09-27 21:55:01 +0300

    started doing var10

--- semester_1/lab3_pointers_and_arrays/lab3_static.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_static.cpp
@@ -1,7 +1,21 @@
+#include <iostream>
 
-// solve task with usage of
-// static arrays
 int main() {
 
+    using std::cout;
+    using std::cin;
+    const int MAX_ARR = 1000;
+    int n;
+    cout << "Enter the number n of array elements: ";
+    cin >> n;
+    if (n > MAX_ARR) {
+        cout << "Wrong input";
+        std::exit(1);
+    }
+    cout << "\n";
+    double arr[MAX_ARR];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
     return 0;
 }
