6fe5c1c611f823fd7dbe572da92b45158264a40c
EgorKlimovec <klimeg147@gmail.com>
2025-10-27 22:39:26 +0300

    redacted

--- semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
@@ -3,14 +3,43 @@
 using std::cin;
 using std::cout;
 
-void arrcout(int* arr, int n) {
+
+void FillArrManually(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        if (!(cin >> arr[i])) {
+            cout << "Wrong input, need to be entered NUMBER (integer or real)" << "\n";
+            delete[] arr;
+            std::exit(1);
+        }
+    }
+}
+
+void FillArrRandomly(int* arr, int n) {
+
+    int a, b, random_number;
+    cout << "Enter numbers a and b (boundaries of random elements): " << "\n";
+    cin >> a >> b;
+    if (a > b) {
+        int temp = b;
+        b = a;
+        a = temp;
+    }
+    std::mt19937 gen(time(0));
+    for (int i = 0; i < n; i++) {
+        std::uniform_int_distribution<int> dist(a, b);
+        random_number = dist(gen);
+        arr[i] = random_number;
+    }
+}
+
+void ArrCout(int* arr, int n) {
 
     for (int i = 0; i < n; i++) {
         cout << arr[i] << "\n";
     }
 }
 
-void productevennum(int* arr, int n) {
+void ProductEvenNum(int* arr, int n) {
 
     long long proisv = 1;
     int ost;
@@ -21,7 +50,7 @@ void productevennum(int* arr, int n) {
     return;
 }
 
-void sumnumfirstlastplus(int* arr, int n) {
+void SumNumFirstLastPlus(int* arr, int n) {
 
     int sum = 0;
     int fst_plus_ind, lst_plus_ind;
@@ -41,7 +70,7 @@ void sumnumfirstlastplus(int* arr, int n) {
     cout << "The sum of the array elements located between the first and last positive elements is:" << sum << "\n";
 }
 
-void sortarr(int* arr, int n) {
+void SortArr(int* arr, int n) {
 
     int j, cur;
     for (int i = 1; i < n; i++) {
@@ -59,11 +88,11 @@ void sortarr(int* arr, int n) {
 
 int main() {
 
-    int n, a, b, random_number;
+    int n;
     char vibor_vvoda;
     cout << "Enter the natural number n: ";
     if (!(cin >> n) || (n < 0)) {
-        cout << "Wrong input";
+        cout << "Wrong input, need to enter integer number, >= 0";
         std::exit(1);
     }
     cout << "\n";
@@ -71,40 +100,27 @@ int main() {
     cout << "Select how the array elements will be created: manually entered - x, randomly - y:";
     cin >> vibor_vvoda;
     if (vibor_vvoda == 'x') {
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
+        FillArrManually(arr, n);
     }
     else if (vibor_vvoda == 'y') {
-        cout << "Enter numbers a and b (boundaries of random elements): " << "\n";
-        cin >> a >> b;
-        if (a > b) {
-            int temp = b;
-            b = a;
-            a = temp;
-        }
-        std::mt19937 gen(time(0));
-        for (int i = 0; i < n; i++) {
-            std::uniform_int_distribution<int> dist(a, b);
-            random_number = dist(gen);
-            arr[i] = random_number;
-        }
+        FillArrRandomly(arr, n);
     }
     else {
-        cout << "wrong input" << "\n";
+        cout << "wrong input, need to be entered symbol x or y" << "\n";
+        delete[] arr;
         std::exit(1);
     }
     cout << "---------------------------------------------------" << "\n";
     cout << "Source array:" << "\n";
-    arrcout(arr, n);
+    ArrCout(arr, n);
     cout << "---------------------------------------------------" << "\n";
-    productevennum(arr, n);
+    ProductEvenNum(arr, n);
     cout << "---------------------------------------------------" << "\n";
-    sumnumfirstlastplus(arr, n);
+    SumNumFirstLastPlus(arr, n);
     cout << "----------------------------------------------------" << "\n";
-    sortarr(arr, n);
+    SortArr(arr, n);
     cout << "Result array:" << "\n";
-    arrcout(arr, n);
+    ArrCout(arr, n);
     delete[] arr;
     return 0;
 }
