288152475afc4de1c857f44b0faac7ab66d319cd
EgorKlimovec <klimeg147@gmail.com>
2025-12-22 22:41:22 +0300

    done

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,29 @@
-
-
+#include <iostream>
+#include <set>
 
 int main() {
+    int N;
+    std::cout << "Введите число N: ";
+    std::cin >> N;
 
-    // your code goes here
+    std::set<int> primes;
+    for (int i = 2; i <= N; i++) {
+        primes.insert(i);
+    }
+
+    for (int i = 2; i * i <= N; i++) {
+        if (primes.find(i) != primes.end()) {
+            for (int j = i * i; j <= N; j += i) {
+                primes.erase(j);
+            }
+        }
+    }
+
+    std::cout << "Простые числа до " << N << ":\n";
+    for (int prime : primes) {
+        std::cout << prime << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
