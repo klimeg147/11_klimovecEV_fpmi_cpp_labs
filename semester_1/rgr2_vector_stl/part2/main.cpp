7effa5b18775080171a911617a05eb9111b1635f
EgorKlimovec <klimeg147@gmail.com>
2025-12-22 21:36:25 +0300

    done

--- semester_1/rgr2_vector_stl/part2/main.cpp
+++ semester_1/rgr2_vector_stl/part2/main.cpp
@@ -2,6 +2,11 @@
 #include "train.h"
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
 
@@ -21,14 +26,123 @@ int main() {
 
     std::cout << "t1: ";
     PrintTime(t1);
-    
+
     std::cout << "t2: ";
     PrintTime(t2);
-    
-    std::cout << "t1 < t2: " << std::boolalpha << (t1 < t2) <<'\n';
+
+    std::cout << "t1 < t2: " << std::boolalpha << (t1 < t2) << '\n';
     std::cout << "t2 < t1: " << std::boolalpha << (t2 < t1) << '\n';
 
+    vector<Train> trains;
+    ifstream file("schedule.txt");
 
+    if (!file.is_open()) {
+        cout << "error: file schedule.txt is not open" << endl;
+        return 1;
+    }
+
+    int id, type_num;
+    std::string dest;
+    int h1, m1, h2, m2;
+
+    while (file >> id >> type_num >> dest >> h1 >> m1 >> h2 >> m2) {
+        TrainType type;
+        if (type_num == 0)
+            type = TrainType::PASSENGER;
+        else if (type_num == 1)
+            type = TrainType::FREIGHT;
+        else if (type_num == 2)
+            type = TrainType::HIGH_SPEED;
+        else if (type_num == 3)
+            type = TrainType::SUBWAY;
+        else
+            type = TrainType::SPECIALIZED;
+
+        time_t dep = SetTime(h1, m1);
+        time_t travel = SetTime(h2, m2);
+
+        trains.push_back(Train(id, type, dest, dep, travel));
+    }
+    file.close();
+
+    if (trains.empty()) {
+        cout << "error: file schedule.txt is empty" << endl;
+        return 1;
+    }
+
+    sort(trains.begin(), trains.end(),
+         [](const Train& a, const Train& b) { return a.GetDispatchTime() < b.GetDispatchTime(); });
+
+    cout << '\n';
+    cout << "Trains after sorting by departure time:" << '\n';
+    for (const auto& train : trains) {
+        train.Print();
+    }
+
+    cout << '\n';
+    cout << "Trains from 13:00 up to 23:00:" << '\n';
+    time_t start = SetTime(13, 0);
+    time_t end = SetTime(23, 0);
+    bool found_time = false;
+
+    for (const auto& train : trains) {
+        time_t t = train.GetDispatchTime();
+        if (t >= start && t <= end) {
+            train.Print();
+            found_time = true;
+        }
+    }
+    if (!found_time)
+        cout << "There are no trains in this time range" << '\n';
+
+    cout << '\n';
+    cout << "Trains to Minsk:" << '\n';
+    bool found_dest = false;
+    for (const auto& train : trains) {
+        if (train.GetDestination() == "Minsk") {
+            train.Print();
+            found_dest = true;
+        }
+    }
+    if (!found_dest) {
+        cout << "There are no trains to Minsk" << '\n';
+    }
+
+    cout << '\n';
+    cout << "Passenger trains to Minsk:" << '\n';
+    bool found_type = false;
+    for (const auto& train : trains) {
+        if (train.GetDestination() == "Minsk" && train.GetType() == TrainType::PASSENGER) {
+            train.Print();
+            found_type = true;
+        }
+    }
+
+    if (!found_type) {
+        cout << "There are no passenger trains to Minsk" << '\n';
+    }
+
+    cout << '\n';
+    cout << "Fastest train to Minsk:" << '\n';
+    const Train* fastest = nullptr;
+    bool found_fastest = false;
+
+    for (const auto& train : trains) {
+        if (train.GetDestination() == "Minsk") {
+            if (!found_fastest) {
+                fastest = &train;
+                found_fastest = true;
+            } else if (train.GetTravellingTime() < fastest->GetTravellingTime()) {
+                fastest = &train;
+            }
+        }
+    }
+
+    if (found_fastest) {
+        fastest->Print();
+    } else {
+        cout << "There are no trains to Minsk" << '\n';
+    }
 
     return EXIT_SUCCESS;
 }
--- semester_1/rgr2_vector_stl/part2/train.h
+++ semester_1/rgr2_vector_stl/part2/train.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 #include "time_utility.h"
 
 using TrainId = size_t;
@@ -21,5 +22,87 @@ private:
     std::time_t travelling_time_;
 
 public:
-    // your code goes here
+    Train()
+        : id_(0),
+          type_(TrainType::PASSENGER),
+          destination_(""),
+          dispatch_time_(0),
+          travelling_time_(0) {
+    }
+
+    Train(TrainId id, TrainType type, const std::string& dest, std::time_t dispatch,
+          std::time_t travelling)
+        : id_(id),
+          type_(type),
+          destination_(dest),
+          dispatch_time_(dispatch),
+          travelling_time_(travelling) {
+    }
+
+    TrainId GetId() const {
+        return id_;
+    }
+
+    TrainType GetType() const {
+        return type_;
+    }
+
+    std::string GetDestination() const {
+        return destination_;
+    }
+
+    std::time_t GetDispatchTime() const {
+        return dispatch_time_;
+    }
+
+    std::time_t GetTravellingTime() const {
+        return travelling_time_;
+    }
+
+    void SetId(TrainId id) {
+        id_ = id;
+    }
+
+    void SetType(TrainType type) {
+        type_ = type;
+    }
+
+    void SetDestination(const std::string& dest) {
+        destination_ = dest;
+    }
+
+    void SetDispatchTime(std::time_t time) {
+        dispatch_time_ = time;
+    }
+
+    void SetTravellingTime(std::time_t time) {
+        travelling_time_ = time;
+    }
+
+    void Print() const {
+        std::cout << "Train " << id_ << ": ";
+        switch (type_) {
+            case TrainType::PASSENGER:
+                std::cout << "Passenger";
+                break;
+            case TrainType::FREIGHT:
+                std::cout << "Cargo";
+                break;
+            case TrainType::HIGH_SPEED:
+                std::cout << "Express";
+                break;
+            case TrainType::SUBWAY:
+                std::cout << "Subway";
+                break;
+            case TrainType::SPECIALIZED:
+                std::cout << "Specialized";
+                break;
+        }
+
+        std::cout << "/ destination: " << destination_;
+        std::cout << "/ dispatch time: ";
+        time_utility::PrintTime(dispatch_time_);
+        std::cout << "/ travelling time: ";
+        time_utility::PrintTime(travelling_time_);
+    }
 };
